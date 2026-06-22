fa34b6360306358cc1fc62505592dc97c21244dd
MrFrozenNik <nikolayrutin@gmail.com>
2026-06-22 07:12:30 +0500

    add nginx certs volume, change database.py default credentials

--- fastapi/database.py
+++ fastapi/database.py
@@ -2,11 +2,11 @@ import os
 import aiomysql
 
 DB_CONFIG = {
-    'host': os.getenv('DB_HOST', '127.0.0.1'),
+    'host': os.getenv('DB_HOST', 'mysql'),
     'port': int(os.getenv('DB_PORT', 3306)),
     'user': os.getenv('DB_USER', 'leanpublic'),
     'password': os.getenv('DB_PASSWORD', 'leanpublic_password'),
-    'db': os.getenv('DB_NAME', 'leanpublic_laravel'),
+    'db': os.getenv('DB_NAME', 'leanpublic'),
     'charset': 'utf8mb4',
 }
 
